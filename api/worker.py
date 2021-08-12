cf5be8dced73300d2717af840ee1e5dd248e1eeb
myejin <rlagpwlsq78@gmail.com>
2021-08-12 23:12:46 +0900

    초기 커밋

---
+++ api/worker.py
@@ -0,0 +1,7 @@
+import os
+from celery import Celery
+
+CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL'),
+CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND')
+
+celery = Celery('tasks', broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND)
