49065a377578499f47679b62c9c6c842a1d6bf12
myejin <rlagpwlsq78@gmail.com>
2021-08-16 23:02:07 +0900

    celery 객체 이름변경
    my_celery

--- api/worker.py
+++ api/worker.py
@@ -4,4 +4,4 @@ from celery import Celery
 CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL'),
 CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND')
 
-celery = Celery('tasks', broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND)
+my_celery = Celery('tasks', broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND)
